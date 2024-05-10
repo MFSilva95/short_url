bd3c3671f1c281f0d2d35bd4625a9cdc2deb05a4
Marina Gonçalves <marinafgoncalves1995@gmail.com>
2024-05-10 22:23:21 +0100

    create model, interface, class and migrations to handle links table;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\LinkRepositoryInterface;
+use App\Repositories\LinkRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind(LinkRepositoryInterface::class, LinkRepository::class);
     }
 
     /**
